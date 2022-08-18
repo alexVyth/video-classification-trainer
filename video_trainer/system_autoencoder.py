49b3eab7087d0abff85dabb9f68e9fd0010593ef
Alexandros Vythoulkas <alvythoulkas@gmail.com>
2022-08-18 19:26:12 +0300

    Change autoencoder model

--- video_trainer/system_autoencoder.py
+++ video_trainer/system_autoencoder.py
@@ -31,8 +31,8 @@ class Autoencoder(lightning.LightningModule):
         self,
     ) -> None:
         super().__init__()
-        self.encoder = models.Encoder2LayerRGB()
-        self.decoder = models.Decoder2LayerRGB()
+        self.encoder = models.Encoder2Layer()
+        self.decoder = models.Decoder2Layer()
         self.accuracy = torchmetrics.Accuracy()
         self.criterion = torch.nn.MSELoss()
 
